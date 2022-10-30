be14a1e7c850943b9124699e88e5a25ef08b2c90
Tagoncalves <tomasalejo2008@hotmail.com>
2022-10-30 16:26:25 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from 'react';
+import { io } from "socket.io-client";
+import {BandAdd} from './components/BandAdd';
+import {BandList} from './components/BandList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const connectSocketServer = () => {
+  const socket = io.connect('http://localhost:8080', {
+    transports: ['websocket']
+  });
+  return socket;
+}
+
+const App = () => {
+
+    const [socket] = useState(connectSocketServer())
+    const [online, setOnline] = useState(false);
+    const [ bands, setBands] = useState([]);
+
+    useEffect(() => {
+
+      setOnline(socket.connected);
+
+    }, [ socket ])
+
+    useEffect(() => {
+
+      socket.on('connect', () => {
+        setOnline(true);
+      });
+
+    }, [ socket ])
+    
+    useEffect(() => {
+
+      socket.on('disconnect', () => {
+        setOnline(false);
+      });
+
+    }, [ socket ])
+    
+    useEffect(() => {
+
+      socket.on('current-bands', (bands) => {
+        setBands(bands);
+      });
+
+    }, [ socket ])
+
+    const votar = ( id ) => {
+      socket.emit('votar-banda', id );
+    }
+
+    const borrar = ( id ) => {
+      socket.emit('borrar-banda', id );
+    }
+
+    const cambiarNombre = ( id, newName ) => {
+      socket.emit('cambiar-nombre-banda', id, newName );
+    }
+
+    const crearBanda = ( nombre ) => {
+      socket.emit('crear-banda', { nombre } );
+    }
+
+
+    return (
+        <div className='container'>
+
+            <div className='alert'>
+                <p>
+                    Service Status:
+                    {
+                      online
+                      ?<span className='text-success'>Online</span>
+                      :<span className='text-danger'>Offline</span>
+                    }
+                </p>
+            </div>
+
+        <h1>BandNames</h1>
+        <hr/>
+
+        <div className='row'>
+          <div className='col-8'>
+              <BandList
+                data={ bands }
+                votar={ votar }
+                borrar={ borrar }
+                cambiarNombre={ cambiarNombre }
+              />
+          </div>
+
+          <div className='col-4'>
+              <BandAdd
+                crearBanda={ crearBanda }
+              />
+          </div>
+        </div>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/BandAdd.js
@@ -0,0 +1,32 @@
+import React,{ useState } from 'react'
+
+export const BandAdd = ( { crearBanda } ) => {
+
+  const [valor, setValor] = useState('')
+
+  const onSubmit = ( ev ) => {
+    ev.preventDefault();
+    
+    if ( valor.trim().length >= 0 ){
+        crearBanda( valor );
+        setValor('');
+    }
+  }
+
+  return (
+    <>
+        <h3>Agregar Banda</h3>
+
+        <form onSubmit={ onSubmit }>
+            <input 
+                className='form-control'
+                placeholder='Nuevo nombre de banda'
+                value={ valor }
+                onChange={ (ev) => setValor( ev.target.value )}
+            />
+
+        </form>
+
+    </>
+  )
+}
